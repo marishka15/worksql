4661ab2de821d3531625a0c9f30a48837c83e1d9
marishka15 <130405170+marishka15@users.noreply.github.com>
2023-07-26 00:46:25 +0300

    Update Script-3.sql

--- Script-3.sql
+++ Script-3.sql
@@ -26,3 +26,21 @@ create table if not EXISTS collection(
 	name VARCHAR(20)not null,
 	year_of_issue DATE not null
 );
+
+create table if not EXISTS genre_executor(
+	genre_executor_id SERIAL PRIMARY KEY,
+	genre_id INTEGER not null references genre(genre_id),
+	executor_id INTEGER not null references executor(executor_id)
+);
+
+create table if not EXISTS album_executor(
+	album_executor_id SERIAL PRIMARY KEY,
+	album_id INTEGER not null references album(album_id),
+	executor_id INTEGER not null references executor(executor_id)
+);
+
+create table if not EXISTS track_collection(
+	track_collection_id SERIAL PRIMARY KEY,
+	track_id INTEGER not null references track(track_id),
+	collection_id INTEGER not null references collection(collection_id)
+);
